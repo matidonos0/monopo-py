a02b0fb08d63b98b108854a684dbcfa2556426f8
matidonos0 <84134331+matidonos0@users.noreply.github.com>
2023-02-02 12:32:47 -0300

    Add files via upload

---
+++ MonopoPY/boxes.py
@@ -0,0 +1,3 @@
+kinds = ['regular', 'tax', 'jail', 'chance', 'chest', 'start', 'freestop']
+
+colors = [None, 'brown', 'cyan', 'pink', 'orange', 'red', 'yellow', 'green', 'blue']
---
+++ MonopoPY/settings.py
@@ -0,0 +1,14 @@
+class Box :
+    def __init__(self, name, cost, color, kind):
+        self.name = name
+        self.cost = cost
+        self.color = color
+        self.kind = kind
+
+class Player :
+    def __init__(self, name, status) :
+        self.name
+        self.status = 0
+        self.balance = 1500
+    #a player can trow dice, buy, auction
+    #a player has to be somewhere in a Box
